dcc747513f82675e47f1f76a5a3a9c54356fbac2
shehanminura <shehanminuradp@gmail.com>
2025-02-22 23:19:17 +0530

    Make Done Customer Form UI

--- MOS_Customer_Frountend/app.js
+++ MOS_Customer_Frountend/app.js
@@ -6,7 +6,7 @@
 // }
 // loadCustomer();
 
-loadCustomer();
+
 function loadCustomer() {
     fetch("http://localhost:8080/customer/getAll")
         .then(res => res.json())
@@ -37,6 +37,39 @@ function loadCustomer() {
             });
             customerTable.innerHTML = tableRow;
         })
+}
+loadCustomer();
+
+function addCustomer() {
+    let name = document.getElementById("txtName").value;
+    let address = document.getElementById("txtAddress").value;
+    let salary = document.getElementById("txtSalary").value;
+
+    console.log(name);
+    console.log(address);
+    console.log(salary);
+    
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+        "name": name,
+        "address": address,
+        "salary": salary
+    });
+
+    const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+    };
+    
+    fetch("http://localhost:8080/customer/add", requestOptions)
+        .then((response) => response.text())
+        .then((result) => console.log(result))
+        .catch((error) => console.error(error));
+   
+
 }
 
 
