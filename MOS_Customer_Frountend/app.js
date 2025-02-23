0c52908255d13b18357c081f4d73bb071954abba
shehanminura <shehanminuradp@gmail.com>
2025-02-23 08:11:52 +0530

    make done CURD function

--- MOS_Customer_Frountend/app.js
+++ MOS_Customer_Frountend/app.js
@@ -6,12 +6,11 @@
 // }
 // loadCustomer();
 
-
 function loadCustomer() {
     fetch("http://localhost:8080/customer/getAll")
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
 
             let tableRow = `
                 <caption>2025 MOS Customers</caption>
@@ -25,7 +24,7 @@ function loadCustomer() {
 
             let customerTable = document.getElementById("tblCustomers");
 
-            data.forEach(customer => {
+            data.forEach((customer) => {
                 tableRow += `    
                 <tr>
                     <td>${customer.id}</td>
@@ -36,7 +35,7 @@ function loadCustomer() {
             `;
             });
             customerTable.innerHTML = tableRow;
-        })
+        });
 }
 loadCustomer();
 
@@ -48,14 +47,14 @@ function addCustomer() {
     console.log(name);
     console.log(address);
     console.log(salary);
-    
+
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
 
     const raw = JSON.stringify({
-        "name": name,
-        "address": address,
-        "salary": salary
+        name: name,
+        address: address,
+        salary: salary,
     });
 
     const requestOptions = {
@@ -63,13 +62,114 @@ function addCustomer() {
         headers: myHeaders,
         body: raw,
     };
-    
+
     fetch("http://localhost:8080/customer/add", requestOptions)
         .then((response) => response.text())
-        .then((result) => console.log(result))
-        .catch((error) => console.error(error));
-   
+        .then((result) => {  
+            alert("Customer added successfully!");
+            console.log(result)
+            loadCustomer();
+            clearFields();
+        })
+        .catch((error) => {
+            alert("Failed to add customer.");
+            console.error(error)});
 
+         
 }
 
+function deleteCustomer() {
+    let id = document.getElementById("txtId").value;
 
+    const requestOptions = {
+        method: "DELETE",
+        redirect: "follow",
+    };
+
+    fetch(`http://localhost:8080/customer/delete/${id}`, requestOptions)
+        .then((response) => response.text())
+        .then((result) =>{
+            alert("Customer Delete successfully!");
+            console.log(result)
+            loadCustomer();
+            clearFields();
+        })
+        .catch((error) =>{ 
+            alert("Failed to Delete customer.");
+            console.error(error)});
+
+     
+}
+
+function seartchCustomer() {
+    let id = document.getElementById("txtId").value;
+    const requestOptions = {
+        method: "GET",
+        redirect: "follow",
+    };
+
+    fetch(`http://localhost:8080/customer/search-by-id/${id}`, requestOptions)
+        .then((response) => response.json())
+        .then((result) => {
+            console.log(result);
+
+            if (result) {
+                document.getElementById("txtId").value = result.id || "";
+                document.getElementById("txtName").value = result.name || "";
+                document.getElementById("txtAddress").value = result.address || "";
+                document.getElementById("txtSalary").value = result.salary || "";
+                alert("Customer Found !");
+
+            }
+        })
+        .catch((error) =>{
+            alert("Failed to customer Found.");
+            console.error(error)
+        } );
+}
+
+function updateCustomer() {
+    let id = document.getElementById("txtId").value;
+    let name = document.getElementById("txtName").value;
+    let address = document.getElementById("txtAddress").value;
+    let salary = document.getElementById("txtSalary").value;
+
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+        id: id,
+        name: name,
+        address: address,
+        salary:  salary ,
+    });
+
+    const requestOptions = {
+        method: "PUT",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
+    };
+
+    fetch("http://localhost:8080/customer/update_Customer", requestOptions)
+        .then((response) => response.text())
+        .then((result) =>{ 
+            alert("Customer Update successfully!");
+            console.log(result)
+            loadCustomer();
+            clearFields();
+        })
+        .catch((error) =>{
+            console.error(error)
+            alert("Failed to Customer Update.");
+
+        });
+    
+}
+
+function clearFields() {
+    document.getElementById("txtId").value = "";
+    document.getElementById("txtName").value = "";
+    document.getElementById("txtAddress").value = "";
+    document.getElementById("txtSalary").value = "";
+}
